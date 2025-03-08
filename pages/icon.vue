02314f5cb2808ccde8e26e2448934db4c95c1fed
Anton Pushkarev <gridmaniac@gmail.com>
2025-03-08 23:10:12 +0400

    chore: update all icons

--- pages/icon.vue
+++ pages/icon.vue
@@ -1,5 +1,43 @@
+<script setup lang="ts">
+const isBorder = ref(false);
+</script>
+
 <template>
-  <div class="card bg-base-200 size-[600px] flex justify-center items-center">
-    <div class="status status-xl scale-2000">星</div>
+  <div class="flex flex-col gap-5 items-center">
+    <div
+      class="card bg-base-200 size-[512px] flex justify-center items-center"
+      :class="{
+        'border border-primary': isBorder,
+      }"
+    >
+      <div class="status status-xl scale-2000">星</div>
+    </div>
+
+    <div
+      class="card bg-base-200 size-[192px] flex justify-center items-center"
+      :class="{
+        'border border-primary': isBorder,
+      }"
+    >
+      <div class="status status-xl scale-700">星</div>
+    </div>
+
+    <div
+      class="card bg-base-200 size-[144px] flex justify-center items-center"
+      :class="{
+        'border border-primary': isBorder,
+      }"
+    >
+      <div class="status status-xl scale-500">星</div>
+    </div>
+
+    <div
+      class="card bg-base-200 size-[64px] flex justify-center items-center"
+      :class="{
+        'border border-primary': isBorder,
+      }"
+    >
+      <div class="status status-xl scale-200">星</div>
+    </div>
   </div>
 </template>
