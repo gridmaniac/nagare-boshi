0ae218a9c10ceb873bc5f31103acc082cfab3981
Anton Pushkarev <gridmaniac@gmail.com>
2025-05-18 14:46:21 +0400

    feat: make status bar translucent

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,10 @@ export default defineNuxtConfig({
           name: "viewport",
           content: "width=device-width, initial-scale=1, user-scalable=no",
         },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
       ],
       link: [
         {
