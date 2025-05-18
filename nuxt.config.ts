24bc493fb4a6165cfce57207d2de0f0d77c45617
Anton Pushkarev <gridmaniac@gmail.com>
2025-05-18 15:50:33 +0400

    chore: more bg experimenting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
       name: "流れ星",
       short_name: "流れ星",
       description: "Leverage Imiwa? backup to study SRS flashcards.",
-      background_color: "#09002f",
+      background_color: "#120b3d",
       theme_color: "#09002f",
       icons: [
         {
