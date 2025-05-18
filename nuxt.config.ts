41e456a264ad0d91912518b16e5009b5f258722a
Anton Pushkarev <gridmaniac@gmail.com>
2025-05-18 15:52:33 +0400

    chore: make both colors same

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,7 +60,7 @@ export default defineNuxtConfig({
       short_name: "流れ星",
       description: "Leverage Imiwa? backup to study SRS flashcards.",
       background_color: "#120b3d",
-      theme_color: "#09002f",
+      theme_color: "#120b3d",
       icons: [
         {
           src: "icons/icon-64x64.png",
