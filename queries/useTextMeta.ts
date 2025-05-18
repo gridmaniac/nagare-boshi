0d64aa9a07ef863913fa3077625ae8d572e04c61
Anton Pushkarev <gridmaniac@gmail.com>
2025-05-19 01:43:42 +0400

    feat: engage caching for jisho meta

--- queries/useTextMeta.ts
+++ queries/useTextMeta.ts
@@ -3,12 +3,21 @@ export const useTextMeta = defineQuery(() => {
 
   const { data: meta, ...query } = useQuery({
     key: () => ["meta", text.value || ""],
-    enabled: () => !!text.value,
+    enabled: () => !!text.value && import.meta.env.PROD,
     staleTime: Infinity,
-    query: async () =>
-      await $fetch<Meta>("/api/text-meta", {
+    query: async () => {
+      const cached = localStorage.getItem(`meta-${text.value}`);
+      if (cached) {
+        return JSON.parse(cached);
+      }
+
+      const res = await $fetch<Meta>("/api/text-meta", {
         params: { text: text.value },
-      }),
+      });
+
+      localStorage.setItem(`meta-${text.value}`, JSON.stringify(res));
+      return res;
+    },
   });
 
   return {
