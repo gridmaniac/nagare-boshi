ecd88e8ecf9fccc2e1dfad44f88b9b9dd79dbf65
Anton Pushkarev <gridmaniac@gmail.com>
2025-05-24 17:49:16 +0400

    fix: remove unnecessary newline in telegram bot message
    This commit removes an unnecessary newline in the Telegram bot message to improve formatting.
    
    - Removes the extra newline character in the Telegram bot's message in `server/api/telegram-webhook.post.ts` to prevent an unwanted gap in the message.

--- server/api/telegram-webhook.post.ts
+++ server/api/telegram-webhook.post.ts
@@ -21,15 +21,7 @@ export default defineEventHandler(async (event) => {
     if (text) {
       await bot.sendMessage(
         chatId,
-        `
-          <b>〜あまり</b> \n
-          <i>喜びのあまり、万歳と叫びたい思いだった。</i> \n
-          I wanted to shout with joy. \n
-          <i>多くの人は自立を求めるあまり、わがままな理由で離婚しようとする。</i> \n
-          Many people leave their marriages for selfish reasons in the name of independence. \n
-          <pre>настолько, что</pre> \n
-          <a href="http://www.google.com/">Show more</a> \n
-        `,
+        `<b>〜あまり</b> \n<i>喜びのあまり、万歳と叫びたい思いだった。</i> \nI wanted to shout with joy. \n<i>多くの人は自立を求めるあまり、わがままな理由で離婚しようとする。</i> \nMany people leave their marriages for selfish reasons in the name of independence. \n<pre>настолько, что</pre> \n<a href="http://www.google.com/">Show more</a> \n`,
         { parse_mode: "HTML" }
       );
     }
